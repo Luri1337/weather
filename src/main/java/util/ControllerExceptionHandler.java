0e4aacb47ccbee5428f466efc981f3aa3ce5e4e7
luri1337 <dkustov1337@gmail.com>
2025-12-07 21:15:16 +0500

    refactoring

--- src/main/java/util/ControllerExceptionHandler.java
+++ src/main/java/util/ControllerExceptionHandler.java
@@ -1,16 +1,28 @@
 package util;
 
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import util.exception.NotUniqueLoginException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handleException(Exception ex, HttpServletRequest request) {
-        System.out.println(ex.getMessage());
+    public String handleException(MethodArgumentNotValidException ex, Model model) {
+        String message = ex.getBindingResult()
+                .getAllErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        model.addAttribute("message", message);
+        return "error";
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleException(RuntimeException ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
         return "error";
     }
 }
